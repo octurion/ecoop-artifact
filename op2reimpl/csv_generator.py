851d42e16a49209ac976f886895ccbfeff728a37
Alexandros Tasos <a.tasos17@imperial.ac.uk>
2019-09-08 14:17:23 +0100

    Add results

--- op2reimpl/csv_generator.py
+++ op2reimpl/csv_generator.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import itertools as it
 
 import csv
@@ -33,11 +34,10 @@ def write_results(f, machines, variants, results):
     for e in zip(*results):
         w.writerow(list(e))
 
-def main(orig_root, ours_root, out_root):
+def main(root):
     num_tries = 20
 
-    #machines = ["desktop", "laptop", "graphic", "ray", "voxel"]
-    machines = ["laptop"]
+    machines = ["desktop", "laptop", "graphic", "ray", "voxel"]
     airfoil_variants = ["orig", "aos", "mixed", "soa"]
     aero_variants = ["orig", "aos", "mixed"]
 
@@ -46,7 +46,7 @@ def main(orig_root, ours_root, out_root):
 
     for machine in machines:
         op2_orig = "op2_orig_{}.txt".format(machine)
-        with open(os.path.join(orig_root, op2_orig), 'r') as f:
+        with open(os.path.join(root, op2_orig), 'r') as f:
             lines = [e for e in filter_op2_orig_file(f)]
             plan_times = lines[0::2]
             execution_times = lines[1::2]
@@ -58,30 +58,19 @@ def main(orig_root, ours_root, out_root):
             aero_results.append(lists[1])
 
         op2_ours = "op2_ours_{}.txt".format(machine)
-        with open(os.path.join(ours_root, op2_ours), 'r') as f:
+        with open(os.path.join(root, op2_ours), 'r') as f:
             lines = [e.strip() for e in filter_op2_ours_file(f)]
             lists = [l for l in chunks(lines, num_tries)]
 
             aero_results.extend(lists[:2])
             airfoil_results.extend(lists[2:])
 
-    with open(os.path.join(out_root, 'airfoil.csv'), 'w') as f:
+    with open(os.path.join(root, 'airfoil.csv'), 'w') as f:
         write_results(f, machines, airfoil_variants, airfoil_results)
 
-    with open(os.path.join(out_root, 'aero.csv'), 'w') as f:
+    with open(os.path.join(root, 'aero.csv'), 'w') as f:
         write_results(f, machines, aero_variants, aero_results)
 
-#    for l in lists:
-#        l = [float(e) for e in l]
-#        print("Mean: {}".format(statistics.mean(l)))
-#        print("Median: {}".format(statistics.median(l)))
-#        print("Stdev: {}".format(statistics.stdev(l)))
-#        print("Min: {}".format(min(l)))
-#        print("Max: {}".format(max(l)))
-
 if __name__ == "__main__":
-    orig_path = sys.argv[1] if len(sys.argv) >= 2 else os.getcwd()
-    ours_path = sys.argv[2] if len(sys.argv) >= 3 else os.getcwd()
-    out_path = os.getcwd()
-    main(orig_path, ours_path, out_path)
-
+    root = sys.argv[1] if len(sys.argv) >= 2 else os.getcwd()
+    main(root)
