bd596a526900c56e3be7a5408a363c14ddea5ed5
Alexandros Tasos <a.tasos17@imperial.ac.uk>
2020-05-02 01:36:44 +0300

    Add original OP2 build commands for the original script

--- build.sh
+++ build.sh
@@ -13,3 +13,8 @@ ninja -C build/
 
 # Extract the OP2 input files
 tar --lzma -xvf op2reimpl/op2_inputs.tar.lzma -C build/op2reimpl/
+
+mkdir op2orig/apps/c/build
+cp build/op2reimpl/new_grid.dat build/op2reimpl/FE_grid.dat op2orig/apps/c/build
+cd op2orig/apps/c/
+./cmake.local -DCMAKE_BUILD_TYPE=Release
