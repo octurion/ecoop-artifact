31c9a27622aca24bc19674fd9dc3fc05476bbb0a
Alexandros Tasos <a.tasos17@imperial.ac.uk>
2020-05-02 01:39:16 +0300

    Have the build script build the original OP2 code, too

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # First build all projects but the original OP2 code
+pushd .
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release\
@@ -8,13 +9,15 @@ cmake -DCMAKE_BUILD_TYPE=Release\
     -DBENCHMARK_ENABLE_GTEST_TESTS=OFF \
     -DBENCHMARK_ENABLE_LTO=ON \
     -GNinja ..
-cd ..
+popd
 ninja -C build/
 
 # Extract the OP2 input files
 tar --lzma -xvf op2reimpl/op2_inputs.tar.lzma -C build/op2reimpl/
 
+pushd .
 mkdir op2orig/apps/c/build
 cp build/op2reimpl/new_grid.dat build/op2reimpl/FE_grid.dat op2orig/apps/c/build
 cd op2orig/apps/c/
 ./cmake.local -DCMAKE_BUILD_TYPE=Release
+popd
